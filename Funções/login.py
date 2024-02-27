7ba90bdba2ff2b787c045082c02052fb55c0a09e
LucasSan1 <137550683+LucasSan1@users.noreply.github.com>
2024-02-26 22:31:39 -0300

    Add files via upload

---
+++ Funções/login.py
@@ -0,0 +1,27 @@
+print('Para o seu cadastro digite seu nome e sua senha')
+
+n = input('Digite seu nome')
+s = input('digite sua senha')
+
+def login():
+  global N
+  global S
+  
+  print('Para logar digite seu nome e senha')
+
+  N = ''
+  S = ''
+
+  while(n != N) or (s != S):
+  
+    N = input('Digite seu nome') 
+    S = input('Digite sua senha')
+
+
+    if n == N and s == S:
+      return ('Aguarde enquanto é logado')
+    else: 
+      return 'Nome ou Senha incorretos'
+  
+
+login()
